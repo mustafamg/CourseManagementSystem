436e4805a30fab40bdab9e8e251df7aaf025b0c2
Mustafa Gamal <mustafa@moveitgate.com>
2014-10-14 09:27:15 +0200

    First commit

---
+++ data/index.js
@@ -0,0 +1,13 @@
+﻿(function(data) {
+	var seedData = require("./seedData");
+	var database = require("./database");
+
+	data.getNotesCategories=function(next) {
+
+	};
+	function seedDatabase() {
+		
+	}
+
+	seedDatabase();
+})(module.exports);

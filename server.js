c0329cb9601051c39e2495a4be395da28d26f288
Mustafa Gamal <mustafa@moveitgate.com>
2014-11-12 10:47:36 +0200

    Tmp Comment to move folder

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , mongoose =require('mongoose')
   , controllers = require("./server/controllers");
 
 var app = express();
@@ -22,18 +21,14 @@ require('./config/express')(app, config);
 
 require('./config/routes')(app);
 
+require('./config/mongoose')(config);
 //app.set('port', process.env.PORT || 3000);
 // development only
 
 //app.get('/', routes.index);
 //app.get('/users', user.list);
 //Database section
-mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console,'Connection error... '));
-db.once('open', function(){
-	console.log('cmsDb opened');
-});
+
 
 http.createServer(app).listen(config.port, function(){ //app.get('port')
   console.log('Express server listening on port ' + app.get('port'));

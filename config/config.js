77a0513368cd53b8ba2564c51d317e0e5fc38114
Mustafa Gamal <mustafa@moveitgate.com>
2015-01-13 14:10:11 +0200

    Update ports in config

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ var rootPath = path.normalize(__dirname + '/../');
 module.exports=
 	{
 		development:{
-			db:'mongodb://bGBRoJbzksdf:kLIAYobQssqx@mongosoup-cont002.mongosoup.de:31436/cc_bGBRoJbzksdf',
+			db:'mongodb://127.0.0.1:27017/cmsDb',
 			rootPath: rootPath,
-			port: process.env.port || 80
+			port: process.env.PORT || 3000
 		},
 		production:{
 			db:'mongodb://bGBRoJbzksdf:kLIAYobQssqx@mongosoup-cont002.mongosoup.de:31436/cc_bGBRoJbzksdf',
 			rootPath: rootPath,
-			port: process.env.port || 80
+			port: process.env.PORT || 80
 		}
 	};

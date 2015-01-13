9992c8380d71e74d067edbcd2b494d13a1af74ca
Mustafa Gamal <mustafa@moveitgate.com>
2015-01-13 14:46:47 +0200

    update to mongodb connection

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ var rootPath = path.normalize(__dirname + '/../');
 module.exports=
 	{
 		development:{
-			db:'mongodb://127.0.0.1:27017/cmsDb',
+			db:'mongodb://bGBRoJbzksdf:kLIAYobQssqx@mongosoup-cont002.mongosoup.de:31436/cc_bGBRoJbzksdf',
 			rootPath: rootPath,
 			port: process.env.PORT || 3000
 		},

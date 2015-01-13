1950dc12072d5a831c62d69111a35afe94faf7b7
Mustafa Gamal <mustafa@moveitgate.com>
2015-01-13 14:17:08 +0200

    Add announce

--- public/app/controllers/services/servicesCtrl.js
+++ public/app/controllers/services/servicesCtrl.js
@@ -1,63 +1,74 @@
 ﻿'use strict';
 /* Controllers */
-    app.register.controller('servicesCtrl', ['$scope','$http', '$location',
-        function ($scope, $http, $location) {
-            //todo: add your view title here
-            $scope.viewTitle = "Items";
-            //todo: Define creat button title
-            $scope.createButtonTitle = "Create Item";
-            //todo: define your edit rout here
-            //dont forget to add rout for the controller in the app.js file
-            var editRoute = "/serviceEdit/";
-            //todo: define your main baseurl of your main api restfull datasource
-            var apiBaseUrl = "/services/";
+app.register.controller('servicesCtrl', ['$scope', '$http', '$location',
+    function ($scope, $http, $location) {
+        //todo: add your view title here
+        $scope.viewTitle = "Items";
+        //todo: Define creat button title
+        $scope.createButtonTitle = "Create Item";
+        //todo: define your edit rout here
+        //dont forget to add rout for the controller in the app.js file
+        var editRoute = "/serviceEdit/";
+        //todo: define your main baseurl of your main api restfull datasource
+        var apiBaseUrl = "/services/";
 
-            $scope.items = [];
-            //Create
-            $scope.new = function () {
-                $location.path(editRoute + "0"); //0 means new items
-            };
-            //Edit
-            $scope.edit = function (item) {
-                console.log(editRoute + item._id);
-                $location.path(editRoute + item._id);
-            };
+        $scope.items = [];
+        //Create
+        $scope.new = function () {
+            $location.path(editRoute + "0"); //0 means new items
+        };
+        //Edit
+        $scope.edit = function (item) {
+            console.log(editRoute + item._id);
+            $location.path(editRoute + item._id);
+        };
 
-            //Delete
-            $scope.delete = function (data) {
-                //Todo: Assign title of your object for delete confirmation
-                var itemTitle = data.title;//Not necessary data.Title
+        //Delete
+        $scope.delete = function (data) {
+            //Todo: Assign title of your object for delete confirmation
+            var itemTitle = data.title;//Not necessary data.Title
 
-                if (confirm("Are you sure you want to delete " + itemTitle + "?"))
-                    $http.delete(apiBaseUrl+ data._id).
-                        //Process delete success
-                        success(function () {
-                            //ui.showMessage(itemTitle + " deleted successfully", "warning");
-                            $scope.items.splice($scope.items.indexOf(data), 1);
-                        }).
-                        //Process delete error
-                        error(function (error) {
-                            //ui.showMessage('Unable to delete ' + itemTitle + ': ' + error.Mssage, 'error');
-                        });
-            };
+            if (confirm("Are you sure you want to delete " + itemTitle + "?"))
+                $http.delete(apiBaseUrl + data._id).
+                    //Process delete success
+                    success(function () {
+                        //ui.showMessage(itemTitle + " deleted successfully", "warning");
+                        $scope.items.splice($scope.items.indexOf(data), 1);
+                    }).
+                    //Process delete error
+                    error(function (error) {
+                        //ui.showMessage('Unable to delete ' + itemTitle + ': ' + error.Mssage, 'error');
+                    });
+        };
 
-            $scope.serviceRequestsDetails= function (item) {
-              $location.path('/serviceRequests/' + item._id);
-            };
-            //List
-            $scope.list = function () {
-                $http.get(apiBaseUrl)
-                    .success(function (data) {
-                        $scope.items = data.serviceList;
-                    })
-                    .error(processLoadError);
-            };
-            //initializing
-            $scope.list();
-            //Todo: process load error here
-            function processLoadError(error) {
-                //ui.showMessage('Unable to load data: ' + error.Message, 'error');
-                alert("Error happened while loading: " + error.Message, "error");
-            };
-        }]);
+        $scope.announce = function (data) {
+
+            $http.get(apiBaseUrl + 'announce/' + data._id).
+                success(function () {
+                    alert("announced");
+                }).
+                error(function (error) {
+                    alert("error:" + error.message);
+                });
+        };
+
+        $scope.serviceRequestsDetails = function (item) {
+            $location.path('/serviceRequests/' + item._id);
+        };
+        //List
+        $scope.list = function () {
+            $http.get(apiBaseUrl)
+                .success(function (data) {
+                    $scope.items = data.serviceList;
+                })
+                .error(processLoadError);
+        };
+        //initializing
+        $scope.list();
+        //Todo: process load error here
+        function processLoadError(error) {
+            //ui.showMessage('Unable to load data: ' + error.Message, 'error');
+            alert("Error happened while loading: " + error.Message, "error");
+        };
+    }]);
 //controller

579e065d365f6bcd3d779aff17b16ad0497367b5
Mustafa M. Gamal <mustafa@moveitgate.com>
2015-06-09 11:43:12 +0200

    Changing db connection to use mongo lab

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/../');
 module.exports=
-	{
-		development:{
-			db:'mongodb://bGBRoJbzksdf:kLIAYobQssqx@mongosoup-cont002.mongosoup.de:31436/cc_bGBRoJbzksdf',
-			rootPath: rootPath,
-			port: process.env.PORT || 3000
-		},
-		production:{
-			db:'mongodb://bGBRoJbzksdf:kLIAYobQssqx@mongosoup-cont002.mongosoup.de:31436/cc_bGBRoJbzksdf',
-			rootPath: rootPath,
-			port: process.env.PORT || 80
-		}
-	};
+{
+	development:{
+		db:'mongodb://webroot:itida@ds031691.mongolab.com:31691/itidacms',
+		rootPath: rootPath,
+		port: process.env.PORT || 3000
+	},
+	production:{
+		db:'mongodb://webroot:itida@ds031691.mongolab.com:31691/itidacms',//'mongodb://bGBRoJbzksdf:kLIAYobQssqx@mongosoup-cont002.mongosoup.de:31436/cc_bGBRoJbzksdf',
+		rootPath: rootPath,
+		port: process.env.PORT || 80
+	}
+};
